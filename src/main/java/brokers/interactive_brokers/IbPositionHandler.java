edadb3c7873c7f30996b7f24e4a059c6547905d9
Benjamin Boyle <benjamin.t.boyle@gmail.com>
2016-03-12 14:29:31 -0800

    Better logging of positions

--- src/main/java/brokers/interactive_brokers/IbPositionHandler.java
+++ src/main/java/brokers/interactive_brokers/IbPositionHandler.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ib.client.ComboLeg;
+import com.ib.client.Contract;
 import com.ib.controller.ApiController.IPositionHandler;
 import com.ib.controller.NewContract;
 
@@ -26,32 +28,32 @@ public class IbPositionHandler implements IPositionHandler, PositionHandler {
 	}
 
 	@Override
-	public synchronized void position(String account, NewContract contract, int position, double avgCost) {
+	public synchronized void position(String account, NewContract newContract, int position, double avgCost) {
 		logger.info(
 				"Handler has received position from Brokers servers: Account[{}], Contract[{}], Position[{}], Average Cost[{}]",
-				account, contract, position, avgCost);
-		switch (contract.secType()) {
+				account, this.contractToString(newContract.getContract()), position, avgCost);
+		switch (newContract.secType()) {
 		case STK:
-			this.portfolioObserver.ingestPosition(new Stock(contract.symbol(), position, avgCost));
+			this.portfolioObserver.ingestPosition(new Stock(newContract.symbol(), position, avgCost));
 			break;
 		case OPT:
-			LocalDate expiration = Option.convertExpiration(contract.expiry());
+			LocalDate expiration = Option.convertExpiration(newContract.expiry());
 
-			switch (contract.right()) {
+			switch (newContract.right()) {
 			case Call:
-				this.portfolioObserver.ingestPosition(
-						new Option(SecurityType.CALL, contract.symbol(), position, contract.strike(), expiration));
+				this.portfolioObserver.ingestPosition(new Option(SecurityType.CALL, newContract.symbol(), position,
+						newContract.strike(), expiration));
 				break;
 			case Put:
 				this.portfolioObserver.ingestPosition(
-						new Option(SecurityType.PUT, contract.symbol(), position, contract.strike(), expiration));
+						new Option(SecurityType.PUT, newContract.symbol(), position, newContract.strike(), expiration));
 				break;
 			default:
 				break;
 			}
 			break;
 		default:
-			logger.error("Can not determine Position Type: {}", contract);
+			logger.error("Can not determine Position Type: {}", this.contractToString(newContract.getContract()));
 			break;
 		}
 	}
@@ -71,5 +73,97 @@ public class IbPositionHandler implements IPositionHandler, PositionHandler {
 	public void requestPositionsFromBrokerage() {
 		logger.info("Requesting Positions from Interactive Brokers");
 		this.controller.getController().reqPositions(this);
+	}
+
+	private String contractToString(Contract contract) {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		stringBuilder.append("Contract Id: ");
+		stringBuilder.append(contract.m_conId);
+
+		stringBuilder.append(", Symbol: ");
+		stringBuilder.append(contract.m_symbol);
+
+		stringBuilder.append(", Security Type: ");
+		stringBuilder.append(contract.m_secType);
+
+		stringBuilder.append(", Expiry: ");
+		stringBuilder.append(contract.m_expiry);
+
+		stringBuilder.append(", Strike: ");
+		stringBuilder.append(contract.m_strike);
+
+		stringBuilder.append(", Right: ");
+		stringBuilder.append(contract.m_right);
+
+		stringBuilder.append(", Multiplier: ");
+		stringBuilder.append(contract.m_multiplier);
+
+		stringBuilder.append(", Exchange: ");
+		stringBuilder.append(contract.m_exchange);
+
+		stringBuilder.append(", Currency: ");
+		stringBuilder.append(contract.m_currency);
+
+		stringBuilder.append(", Local Symbol: ");
+		stringBuilder.append(contract.m_localSymbol);
+
+		stringBuilder.append(", Trading Class: ");
+		stringBuilder.append(contract.m_tradingClass);
+
+		stringBuilder.append(", Primary Exchange: ");
+		stringBuilder.append(contract.m_primaryExch);
+
+		stringBuilder.append(", Include Expired: ");
+		stringBuilder.append(contract.m_includeExpired);
+
+		stringBuilder.append(", Security Id Type: ");
+		stringBuilder.append(contract.m_secIdType);
+
+		stringBuilder.append(", Security Id: ");
+		stringBuilder.append(contract.m_secId);
+
+		stringBuilder.append(", Combo Leg Description: ");
+		stringBuilder.append(contract.m_comboLegsDescrip);
+
+		stringBuilder.append(", Combo Legs: { ");
+		for (ComboLeg leg : contract.m_comboLegs) {
+			stringBuilder.append("[Contract Id: ");
+			stringBuilder.append(leg.m_conId);
+
+			stringBuilder.append(", Ratio: ");
+			stringBuilder.append(leg.m_ratio);
+
+			stringBuilder.append(", Action: ");
+			stringBuilder.append(leg.m_action);
+
+			stringBuilder.append(", Exchange: ");
+			stringBuilder.append(leg.m_exchange);
+
+			stringBuilder.append(", Open/Close: ");
+			stringBuilder.append(leg.m_openClose);
+
+			stringBuilder.append(", Short Sale Slot: ");
+			stringBuilder.append(leg.m_shortSaleSlot);
+
+			stringBuilder.append(", Designated Location: ");
+			stringBuilder.append(leg.m_designatedLocation);
+
+			stringBuilder.append(", Exempt Code: ");
+			stringBuilder.append(leg.m_exemptCode);
+
+			stringBuilder.append("]");
+		}
+
+		stringBuilder.append(" }, Under Comp: { ");
+		stringBuilder.append("Contract Id: ");
+		stringBuilder.append(contract.m_underComp.m_conId);
+		stringBuilder.append(", Delta: ");
+		stringBuilder.append(contract.m_underComp.m_delta);
+		stringBuilder.append(", Price: ");
+		stringBuilder.append(contract.m_underComp.m_price);
+		stringBuilder.append(" }");
+
+		return stringBuilder.toString();
 	}
 }
