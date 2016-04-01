11f30234bf77bfd01d253f21ae8af395832b1c88
Benjamin Boyle <benjamin.t.boyle@gmail.com>
2016-03-31 21:35:24 -0700

    Updates to PortfolioManager; does not compile

--- src/main/java/theta/portfolio/manager/PortfolioManager.java
+++ src/main/java/theta/portfolio/manager/PortfolioManager.java
@@ -3,6 +3,7 @@ package theta.portfolio.manager;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -58,15 +59,18 @@ public class PortfolioManager implements PortfolioObserver, PositionProvider, Ru
 	}
 
 	private void processPosition(Security security) {
-		// Update security list
-		// process security list
 
 		// If security changed from previous value associated with Security's Id
 		if (!security.equals(this.securityIdMap.put(security.getId(), security))) {
+
 			// If security is mapped to a position, remove position and
 			// position-security map entry
 			if (this.positionSecurityMap.containsKey(security.getId())) {
-				this.positionMap.remove(this.positionSecurityMap.remove(security.getId()));
+				ThetaTrade theta = this.positionMap.remove(this.positionSecurityMap.remove(security.getId()));
+				if (!this.positionMap.values().parallelStream()
+						.filter(ticker -> ticker.getTicker().equals(theta.getTicker())).findAny().isPresent()) {
+					this.monitor.deleteMonitor(theta);
+				}
 			}
 
 			// process securities into positions
@@ -76,13 +80,28 @@ public class PortfolioManager implements PortfolioObserver, PositionProvider, Ru
 							Collectors.groupingBy(Security::getTicker, Collectors.groupingBy(Security::getPrice))));
 			for (String ticker : unassignedSecurities.get(SecurityType.CALL).keySet()) {
 
-				List<Security> stockList = unassignedSecurities.get(SecurityType.STOCK).get(ticker).entrySet().stream().map(price -> price.getValue()).m
+				// All stocks unassigned stocks for specific ticker
+				List<Security> stockList = unassignedSecurities.get(SecurityType.STOCK).get(ticker).entrySet().stream()
+						.map(Entry::getValue).flatMap(List::stream).collect(Collectors.toList());
 
-				for (Double callPrice : unassignedSecurities.get(SecurityType.CALL).get(ticker).keySet()) {
-					List<Security> callList = unassignedSecurities.get(SecurityType.CALL).get(ticker).get(callPrice);
-					List<Security> putList = unassignedSecurities.get(SecurityType.PUT).get(ticker).get(callPrice);
-				
-					
+				// if there is enough stock
+				if (stockList.stream().mapToInt(stock -> stock.getQuantity()).sum() >= 100) {
+					// For each price level of call options
+					for (Double callPrice : unassignedSecurities.get(SecurityType.CALL).get(ticker).keySet()) {
+						List<Security> callListAtPrice = unassignedSecurities.get(SecurityType.CALL).get(ticker)
+								.get(callPrice);
+						List<Security> putListAtPrice = unassignedSecurities.get(SecurityType.PUT).get(ticker)
+								.get(callPrice);
+						// at this point we know stock and call are valid, but
+						// not put
+						if (putListAtPrice.size() > 0) {
+							Stock stock = new Stock(stockList.get(0).getId(), stockList.get(0).getTicker(),
+									stockList.get(0).getQuantity(), stockList.get(0).getPrice());
+							Option call = new Option(callListAtPrice.get(0).getId(), SecurityType.CALL, callListAtPrice.get(0).getTicker(), callListAtPrice.get(0).getQuantity(), callListAtPrice.get(0).getPrice(), callListAtPrice.get(0))
+							ThetaTrade theta = ThetaTrade.of(stockList.get(0), callListAtPrice.get(0),
+									putListAtPrice.get(0));
+						}
+					}
 				}
 			}
 		}
