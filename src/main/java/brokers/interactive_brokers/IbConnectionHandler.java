b33ce3b71ba113b859821926d17e18c6a6ac52e8
Benjamin Boyle <benjamin.t.boyle@gmail.com>
2016-04-07 21:15:37 -0700

    Removed warning messages from broker logging

--- src/main/java/brokers/interactive_brokers/IbConnectionHandler.java
+++ src/main/java/brokers/interactive_brokers/IbConnectionHandler.java
@@ -55,8 +55,6 @@ public class IbConnectionHandler implements IConnectionHandler, IbController, Co
 	public void message(int id, int messageCode, String message) {
 		if (messageCode >= 2100 && messageCode <= 2110) {
 			logger.warn("Interactive Brokers Message - Id: '{}', Code: '{}', Message: '{}'", id, messageCode, message);
-		} else if (messageCode >= 1100 && messageCode <= 1102 || messageCode == 1300) {
-			logger.info("Interactive Brokers Message - Id: '{}', Code: '{}', Message: '{}'", id, messageCode, message);
 		} else {
 			logger.error("Interactive Brokers Message - Id: '{}', Code: '{}', Message: '{}'", id, messageCode, message);
 		}
