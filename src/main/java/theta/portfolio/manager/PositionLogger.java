520b2f13e9d5fbd092af67e8816d1ab64840ebdc
Benjamin Boyle <benjamin.t.boyle@gmail.com>
2018-01-16 09:18:03 -0800

    Fixed position print order

--- src/main/java/theta/portfolio/manager/PositionLogger.java
+++ src/main/java/theta/portfolio/manager/PositionLogger.java
@@ -12,15 +12,16 @@ import theta.domain.api.Security;
 import theta.domain.api.SecurityType;
 
 public class PositionLogger {
-  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private static Comparator<Security> byTicker = Comparator.comparing(Security::getTicker);
 
   private static Comparator<Security> byStockIsGreaterThanOptions = (s1, s2) -> {
     if (s1.getSecurityType() == SecurityType.STOCK) {
-      return 1;
-    } else if (s2.getSecurityType() == SecurityType.STOCK) {
       return -1;
+    } else if (s2.getSecurityType() == SecurityType.STOCK) {
+      return 1;
     } else {
       return 0;
     }
@@ -30,25 +31,28 @@ public class PositionLogger {
 
   private static Comparator<Security> byCallIsGreaterThanPut = (s1, s2) -> {
     if (s1.getSecurityType() == SecurityType.CALL && s2.getSecurityType() == SecurityType.PUT) {
-      return 1;
-    } else if (s1.getSecurityType() == SecurityType.PUT && s2.getSecurityType() == SecurityType.CALL) {
       return -1;
+    } else if (s1.getSecurityType() == SecurityType.PUT
+        && s2.getSecurityType() == SecurityType.CALL) {
+      return 1;
     } else {
       return 0;
     }
   };
 
   public static void logThetaPositions(Collection<ThetaTrade> thetas) {
-    for (final ThetaTrade position : thetas.stream().sorted(Comparator.comparing(ThetaTrade::getTicker))
-        .collect(Collectors.toList())) {
+    for (final ThetaTrade position : thetas.stream()
+        .sorted(Comparator.comparing(ThetaTrade::getTicker)).collect(Collectors.toList())) {
       logger.info("Current position: {}", position);
     }
   }
 
-  public static void logUnmatchedPositions(Collection<Security> allSecurities, Collection<UUID> matchedPositionIds) {
+  public static void logUnmatchedPositions(Collection<Security> allSecurities,
+      Collection<UUID> matchedPositionIds) {
     for (final Security security : allSecurities.stream()
-        .filter(security -> !matchedPositionIds.contains(security.getId())).sorted(byTicker
-            .thenComparing(byStockIsGreaterThanOptions).thenComparing(byPrice).thenComparing(byCallIsGreaterThanPut))
+        .filter(security -> !matchedPositionIds.contains(security.getId()))
+        .sorted(byTicker.thenComparing(byStockIsGreaterThanOptions).thenComparing(byPrice)
+            .thenComparing(byCallIsGreaterThanPut))
         .collect(Collectors.toList())) {
       logger.info("Current unprocessed security: {}", security);
     }
