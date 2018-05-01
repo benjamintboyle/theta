9847319f83608ac009ce5212661acb02e04afcdb
Benjamin Boyle <benjamin.t.boyle@gmail.com>
2018-05-01 16:54:15 -0700

    Updates to gradle.build, added junit 4 & 5 dependencies
    Updated Graddle wrapper to version 4.7.  Added versions for
    dependencies.  Added dependencies for junit, so both junit 4 & 5 tests
    can be used.

--- src/test/java/theta/execution/factory/ExecutableOrderFactoryTest.java
+++ src/test/java/theta/execution/factory/ExecutableOrderFactoryTest.java
@@ -1,16 +1,19 @@
 package theta.execution.factory;
 
-import static org.junit.Assert.fail;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class ExecutableOrderFactoryTest {
 
+  @Disabled
   @Test
   public void testReverseAndValidateStockPositionOrderStockExecutionTypeOptionalOfDouble() {
 
     fail("Not yet implemented");
   }
 
+  @Disabled
   @Test
   public void testReverseAndValidateStockPositionOrderThetaExecutionType() {
     fail("Not yet implemented");
